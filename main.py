496afa943329704646ced92310aa672d1ae363c0
nasrin pathan <nasrinbegump@gmail.com>
2025-09-19 03:17:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')  # Renders the index.html file
 
+# About Page
+@app.route('/about')
+def about():
+    return render_template('about.html')  # Renders about.html
+
+# Contact Page
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  # Renders contact.html
+
 if __name__ == '__main__':
     app.run(debug=True)
